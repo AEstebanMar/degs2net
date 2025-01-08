6236b5c8556ada67882a47603af0fb6f846ad2f8
lhurtadogarcia <luciabioinf@gmail.com>
2025-01-08 15:42:41 +0100

    Began refactoring to a proper daemon

--- launch.sh
+++ launch.sh
@@ -1,25 +1,25 @@
 #! /usr/bin/env bash
 
-#SBATCH --mem='60gb'
-#SBATCH --constraint=cal
-#SBATCH --time='10:00:00'
-hostname
+source ~soft_bio_267/initializes/init_autoflow
+module=$1
+config_daemon=$current_dir/config_daemon
+source $config_daemon
+db_path=$current_dir/databases
 
-#source ~soft_bio_267/initializes/init_netanalyzer
-source ~soft_bio_267/initializes/init_python
-
-db_path="/mnt/home/users/bio_267_uma/lhurtado/test/databases"
-
-#mkdir -p $db_path
-
-#wget "https://string-db.org/cgi/download?sessionId=bhYDip8oIL59/9606.protein.links.v12.0.txt.gz" -O $db_path'/9606.protein.links.v12.0.txt.gz'
-#gzip -d $db_path'/9606.protein.links.v12.0.txt.gz'
-
+if [ "$module" == "0" ] ; then
+	echo "Downloading database"
+	mkdir -p $db_path
+	# We will first download the protein-protein interaction database from STRING. We will then change STRING IDs to ENSEMBL IDs to create a dictionary ENSEMBL-protein to ENSEMBL-gene.
+	wget "https://string-db.org/cgi/download?sessionId=bhYDip8oIL59/9606.protein.links.v12.0.txt.gz" -O $db_path'/9606.protein.links.v12.0.txt.gz'
+	gzip -d $db_path'/9606.protein.links.v12.0.txt.gz'
+fi
 
 stringDB=$db_path'/9606.protein.links.v12.0.txt'
-#cut -f 1,2,3 $stringDB > string_parsed_file.txt # en este archivo particular no es necesario porque solo tiene 3 columnas
 
-#Cambiar códigos de STRING a ENSEMBLID
+source ~soft_bio_267/initializes/init_netanalyzer
+source ~soft_bio_267/initializes/init_python
+
+# Changing STRING IDs to ENSEMBL IDs
 
 #wget "https://string-db.org/cgi/download?sessionId=bhYDip8oIL59/9606.protein.aliases.v12.0.txt.gz" -O $db_path'/9606.protein.aliases.v12.0.txt.gz'
 #gzip -d $db_path'/9606.protein.aliases.v12.0.txt.gz'
@@ -30,15 +30,23 @@ grep 'Ensembl_gene' $db_path'/9606.protein.aliases.v12.0.txt' > $db_path'/dictio
 
 dict=$db_path'/dictionary_ENSP_ENSG'
 
+# We now have our dictionary ready. We will now use standard_name_replacer to translate the string interactome database into ENSEMBL genes IDs.
+
 standard_name_replacer -i $stringDB -I $dict -s ' ' -c 1,2 -f 1 -t 2 | sed -E "s/ /\t/g" | tail -n+2 > $db_path'/nodes_score.txt'
 
 #standard_name_replacer -i 9606.protein.links.v12.0.txt -I dictionary_ENSP_ENSG -s ' ' -c 1,2 -f 1 -t 2 | sed -E "s/ /\t/g" | tail -n+2 > nodes_score.txt
+
+# We now create the interactome  
 
 text2binary_matrix -i $db_path'/nodes_score.txt' -t pair -O bin -o net
 
 #text2binary_matrix -i nodes_score.txt -t pair -O bin -o net
 
+# To extract a subgraph from the protein interactome, we provide a list of prevalent genes from the project we are analyzing
+
 extract_subgraph.py -i net.npy -n net.lst -s DEG_list -o subgraph.txt
+
+# We now apply netanalyzer to generate clusters of genes: 
 
 netanalyzer -i subgraph.txt -f pair -b louvain
 
@@ -55,4 +63,4 @@ source ~soft_bio_267/initializes/init_degenes_hunter # mejor aquí o arriba con 
 clusters_to_enrichment.R -i $db_path'/clusters_aggregated.txt' -w 16 -o functional_results -f MF,BP,CC,KEGG,Reactome -k ENSEMBL -O "Human"
 
 #sed -E "s/ /\t/g" results.txt > nodes_score.txt
-exit
+
