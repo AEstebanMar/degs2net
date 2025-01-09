403d9f4611ba78076908bcbb0277c829965d020c
lhurtadogarcia <luciabioinf@gmail.com>
2025-01-09 16:17:25 +0100

    Finished module 0

--- daemon.sh
+++ daemon.sh
@@ -1,42 +1,49 @@
 #! /usr/bin/env bash
 
 source ~soft_bio_267/initializes/init_autoflow
+source ~soft_bio_267/initializes/init_netanalyzer
+source ~soft_bio_267/initializes/init_python
 module=$1
+current_dir=`pwd`
 config_daemon=$current_dir/config_daemon
 aux_sh=$current_dir/aux_sh
 source $config_daemon
-export PATH=$aux_sh:$PATH
 export db_path=$current_dir/databases
+export download_path=$db_path/download
+
+
+# Module 0 will download the databases, process them and finish by creating the interactome.
 
 if [ "$module" == "0" ] ; then
 	echo "Downloading databases"
 	mkdir -p $db_path
 	# We will first download the protein-protein interaction database from STRING. We will then change STRING IDs to ENSEMBL IDs to create a dictionary ENSEMBL-protein to ENSEMBL-gene.
-	wget "https://string-db.org/cgi/download?sessionId=bhYDip8oIL59/9606.protein.links.v12.0.txt.gz" -O $db_path'/9606.protein.links.v12.0.txt.gz'
-	gzip -d $db_path'/9606.protein.links.v12.0.txt.gz'
-	wget "https://string-db.org/cgi/download?sessionId=bhYDip8oIL59/9606.protein.aliases.v12.0.txt.gz" -O $db_path'/9606.protein.aliases.v12.0.txt.gz'
-	gzip -d $db_path'/9606.protein.aliases.v12.0.txt.gz'
+
+	wget "https://stringdb-downloads.org/download/protein.links.full.v12.0/9606.protein.links.full.v12.0.txt.gz" -O $download_path'/9606.protein.links.v12.0.txt.gz'
+	gzip -d $download_path'/9606.protein.links.v12.0.txt.gz'
+	wget "https://stringdb-downloads.org/download/protein.aliases.v12.0/9606.protein.aliases.v12.0.txt.gz" -O $download_path'/9606.protein.aliases.v12.0.txt.gz'
+	gzip -d $download_path'/9606.protein.aliases.v12.0.txt.gz'
 	# Changing STRING IDs to ENSEMBL IDs
-	grep 'Ensembl_gene' $db_path'/9606.protein.aliases.v12.0.txt' > $db_path'/dictionary_ENSP_ENSG'
-	#grep 'Ensembl_gene' 9606.protein.aliases.v12.0.txt > dictionary_ENSP_ENSG
+	cut -f 1,2,16 -d ' ' $download_path'/9606.protein.links.v12.0.txt' > $db_path'/human.protein.links.v12.0.txt' # columns that refer to protein1, protein2 and combined_score
+	grep 'Ensembl_gene' $download_path'/9606.protein.aliases.v12.0.txt' > $db_path'/dictionary_ENSP_ENSG'
 	dict=$db_path'/dictionary_ENSP_ENSG'
 	# We now have our dictionary ready. We will now use standard_name_replacer to translate the string interactome database into ENSEMBL genes IDs.
-	standard_name_replacer -i $stringDB -I $dict -s ' ' -c 1,2 -f 1 -t 2 | sed -E "s/ /\t/g" | tail -n+2 > $db_path'/nodes_score.txt'
-	#standard_name_replacer -i 9606.protein.links.v12.0.txt -I dictionary_ENSP_ENSG -s ' ' -c 1,2 -f 1 -t 2 | sed -E "s/ /\t/g" | tail -n+2 > nodes_score.txt
-	sbatch launch_text2binary_matrix
+	echo 'Replacing names'
+	standard_name_replacer -i $db_path'/human.protein.links.v12.0.txt' -I $dict -s ' ' -c 1,2 -f 1 -t 2 | sed -E "s/ /\t/g" | tail -n+2 > $db_path'/nodes_score.txt'
+	echo 'Launching text2binary'
+	sbatch $aux_sh'/launch_text2binary_matrix'
+	echo 'Done :)'
 fi
 
 # We now create the interactome  
 
 
-stringDB=$db_path'/9606.protein.links.v12.0.txt'
 
 if [ "$module" == "1" ]; then
-	echo "Processing databases"
+	echo "Obtaining DEG_list"
 
 
-source ~soft_bio_267/initializes/init_netanalyzer
-source ~soft_bio_267/initializes/init_python
+
 
 
 #text2binary_matrix -i nodes_score.txt -t pair -O bin -o net
