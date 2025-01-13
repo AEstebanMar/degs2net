7f48f9e95fb739f21ed1c87d97bd42c36d62b792
lhurtadogarcia <luciabioinf@gmail.com>
2025-01-13 16:31:11 +0100

    Completely implemented daemon structure, and fully uses AutoFlow

--- daemon.sh
+++ daemon.sh
@@ -1,13 +1,17 @@
 #! /usr/bin/env bash
 
-source ~soft_bio_267/initializes/init_autoflow
-source ~soft_bio_267/initializes/init_netanalyzer
-source ~soft_bio_267/initializes/init_python
+
 module=$1
+mode=$2
+aux_opt=$3
 current_dir=`pwd`
 aux_sh=$current_dir/aux_sh
+config_daemon=$current_dir/config_daemon
+source $config_daemon
+source ~soft_bio_267/initializes/init_autoflow
+source ~soft_bio_267/initializes/init_python
 export db_path=$current_dir/databases
-export download_path=$db_path/download
+download_path=$db_path/download
 
 
 # Module 0 will download the databases, process them and finish by creating the interactome.
@@ -16,7 +20,6 @@ if [ "$module" == "0" ] ; then
 	echo "Downloading databases"
 	mkdir -p $db_path
 	# We will first download the protein-protein interaction database from STRING. We will then change STRING IDs to ENSEMBL IDs to create a dictionary ENSEMBL-protein to ENSEMBL-gene.
-
 	wget "https://stringdb-downloads.org/download/protein.links.full.v12.0/9606.protein.links.full.v12.0.txt.gz" -O $download_path'/9606.protein.links.v12.0.txt.gz'
 	gzip -d $download_path'/9606.protein.links.v12.0.txt.gz'
 	wget "https://stringdb-downloads.org/download/protein.aliases.v12.0/9606.protein.aliases.v12.0.txt.gz" -O $download_path'/9606.protein.aliases.v12.0.txt.gz'
@@ -33,38 +36,37 @@ if [ "$module" == "0" ] ; then
 	echo 'Done :)'
 fi
 
-# We now create the interactome  
-
-
-
 if [ "$module" == "1" ]; then
-	echo "Obtaining DEG_list"
+	file_paths=`cat $tracker`
+	for file_path in "$file_paths"
+	do
+		input_path=`echo "$file_path" | cut -f 1`
+		name=`echo "$file_path" | cut -f 2`
+        variables=`echo -e "
+        	\\$input_file=$input_path,
+        	\\$db_path=$db_path
+        " | tr -d '[:space:]' `
 
 
 
+        if [ "$mode" == "exec" ] ; then
+			echo Launching main workflow
+			AutoFlow -w $current_dir/templates/degs2net.af -m '10gb' -c 1 -n 'cal' -V $variables $aux_opt -o $current_dir/exec_$name -e -L
+		elif [ "$mode" == "check" ] ; then
+			flow_logger -w -e $current_dir/exec_$name -r all
+		elif [ "$mode" == "rescue" ] ; then
+			echo Regenerating code
+			AutoFlow -w $template -V $variables $aux_opt -o $current_dir/exec_$name -v
+			echo Launching pending and failed jobs
+			flow_logger -w -e $current_dir/exec_$name -l -p -b
+		fi
 
-
-#text2binary_matrix -i nodes_score.txt -t pair -O bin -o net
-
-# To extract a subgraph from the protein interactome, we provide a list of prevalent genes from the project we are analyzing
-
-extract_subgraph.py -i net.npy -n net.lst -s DEG_list -o subgraph.txt
-
-# We now apply netanalyzer to generate clusters of genes: 
-
-netanalyzer -i subgraph.txt -f pair -b louvain
-
-# we get the following file: louvain_discovered_clusters.txt. We will then use aggregate_column_data from cmdtabs to get each ID cluster 
-# followed by all of its genes separated by comas. 
-
-aggregate_column_data -i louvain_discovered_clusters.txt -x 1 -a 2 -s ',' > $db_path'/clusters_aggregated.txt'
-
-#aggregate_column_data -i louvain_discovered_clusters.txt -x 1 -a 2 -s ',' > clusters_aggregated.txt
-
-# Once clusters are aggregated, we will use clusters_to_enrichment.R to do a functional analysis:
-source ~soft_bio_267/initializes/init_degenes_hunter # mejor aquí o arriba con los demás?
-
-clusters_to_enrichment.R -i $db_path'/clusters_aggregated.txt' -w 16 -o functional_results -f MF,BP,CC,KEGG,Reactome -k ENSEMBL -O "Human"
-
-#sed -E "s/ /\t/g" results.txt > nodes_score.txt
-
+	done
+	
+	# # we get the following file: louvain_discovered_clusters.txt. We will then use aggregate_column_data from cmdtabs to get each ID cluster 
+	# # followed by all of its genes separated by comas. 
+	# aggregate_column_data -i louvain_discovered_clusters.txt -x 1 -a 2 -s ',' > $db_path'/clusters_aggregated.txt'
+	# # Once clusters are aggregated, we will use clusters_to_enrichment.R to do a functional analysis:
+	# source ~soft_bio_267/initializes/init_degenes_hunter # mejor aquí o arriba con los demás?
+	# clusters_to_enrichment.R -i $db_path'/clusters_aggregated.txt' -w 16 -o functional_results -f MF,BP,CC,KEGG,Reactome -k ENSEMBL -O "Human"
+fi
